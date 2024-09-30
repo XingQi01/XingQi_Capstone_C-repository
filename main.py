253d42b5cfc38ae7e27506ae864e0fd7a3b28db6
XingQi01 <xingqiteo@gmail.com>
2024-09-30 22:18:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,30 +19,31 @@ st.title("HDB Resale Flat Finder")
 if not check_password():  
     st.stop()
 
+st.subheader("You can use this query below to find available HDB resale flats in Singapore, by querying the location and price range. The table output will show you information related to the HDB resale flats based on your query.")
+
 form = st.form(key="form")
-form.subheader("You can use this query below to find available HDB resale flats in Singapore, by querying the location and price range. The table output will show you information related to the HDB resale flats based on your query.")
 
 user_prompt = form.text_area("E.g. I am looking for HDB resale flats in Tampines. I want to buy HDB resale flats from Bukit Panjang and my budget is SGD 500,000. I want to buy 16th floor unit in Ang Mo Kio, and my budget is SGD 300,000", height=100)
 
-# Check if the submit button is working
+    # Check if the submit button is working
 if form.form_submit_button("Submit"):
-    st.divider()
+        st.divider()
 
-    try:
-        # Process the user message
-        response = process_user_message(user_prompt)
-        st.write("Please refer to the results below:")
-        st.write(response)
+        try:
+            # Process the user message
+            response = process_user_message(user_prompt)
+            st.write("Please refer to the results below:")
+            st.write(response)
         
-        # Filter the DataFrame based on the user input
-        filtered_towns = filter_df(user_prompt)
-        filtered_df = df_hdb_resale[df_hdb_resale['town'].isin(filtered_towns)]
+            # Filter the DataFrame based on the user input
+            filtered_towns = filter_df(user_prompt)
+            filtered_df = df_hdb_resale[df_hdb_resale['town'].isin(filtered_towns)]
         
-        st.write("Other units to consider:")
-        st.write(filtered_df)
-    except Exception as e:
-        st.error(f"Error processing your request: {e}")
+            st.write("Other units to consider:")
+            st.write(filtered_df)
+        except Exception as e:
+            st.error(f"Error processing your request: {e}")
     
-    st.divider()
+        st.divider()
 else:
-    st.write("Form not yet submitted")  # Just for checking if the button logic is working correctly.
+        st.write("Form not yet submitted")  # Just for checking if the button logic is working correctly.
