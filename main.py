3058124033efee543a8a35c2f5635e3b8582c009
XingQi01 <xingqiteo@gmail.com>
2024-10-01 21:28:54 +0800

    13

--- main.py
+++ main.py
@@ -19,11 +19,20 @@ st.title("HDB Resale Flat Finder")
 if not check_password():  
     st.stop()
 
-st.subheader("You can use this query below to find available HDB resale flats in Singapore, by querying the location and price range. The table output will show you information related to the HDB resale flats based on your query.")
+# Create a subheader with smaller text size
+st.subheader("You can use this query below to find available HDB resale flats in Singapore:")
+
+# Add a smaller description text under the subheader
+st.markdown("""
+<p style='font-size:14px'>
+By querying information such as location, price, price range, street name, floor and floor range, 
+the table output will show you information related to the HDB resale flats based on your query. If you have a query on the location but did not have an output, please refer to the list of locations under the town column in hte View All HDB resale wef 2017 page.
+</p>
+""", unsafe_allow_html=True)
 
 form = st.form(key="form")
 
-user_prompt = form.text_area("E.g. I am looking for HDB resale flats in Tampines. I want to buy HDB resale flats from Bukit Panjang and my budget is SGD 500,000. I want to buy 16th floor unit in Ang Mo Kio, and my budget is SGD 300,000", height=100)
+user_prompt = form.text_area("E.g. I am looking for HDB resale flats in Tampines. I want to buy HDB resale flats from Bukit Panjang and my budget is SGD 500,000. I want to buy 16th floor unit in Ang Mo Kio, and my budget is SGD 300,000.", height=100)
 
     # Check if the submit button is working
 if form.form_submit_button("Submit"):
